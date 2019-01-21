d4e01562720d1a68ab8ac108d91f9351025da61c
Roshan Rijal <rijalroshan94@gmail.com>
2019-01-21 16:20:50 +0545

    first commit

---
+++ app/src/main/java/com/rowsun/myapplication/WebActivityActivity.java
@@ -0,0 +1,13 @@
+package com.rowsun.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_activity);
+    }
+}
